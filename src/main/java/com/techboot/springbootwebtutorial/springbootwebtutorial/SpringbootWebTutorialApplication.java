3019deca680a41ba8d5a1ed9854bdfe85570178b
vinay maurya <vinaymaurya06072000@gmail.com>
2025-02-21 08:25:55 +0530

    chapter 2.1

---
+++ src/main/java/com/techboot/springbootwebtutorial/springbootwebtutorial/SpringbootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.techboot.springbootwebtutorial.springbootwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techboot/springbootwebtutorial/springbootwebtutorial/SpringbootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techboot.springbootwebtutorial.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
